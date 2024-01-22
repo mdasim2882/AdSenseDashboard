086cdb1a6e08ec5537f19b2b5a9ecaabf81a37ea
mdasim2882 <mohdasim2882@gmail.com>
2024-01-23 01:47:14 +0530

    added bottom navigation view

---
+++ app/src/main/java/com/example/adsensedashboard/ui/data/ConsoleUiActivity.kt
@@ -0,0 +1,12 @@
+package com.example.adsensedashboard.ui.data
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.adsensedashboard.R
+
+class ConsoleUiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_console_ui)
+    }
+}
