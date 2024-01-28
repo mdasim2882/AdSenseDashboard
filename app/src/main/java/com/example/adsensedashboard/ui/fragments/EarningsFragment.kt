ce82666bee692f93ea296719ac5a24716b0122c0
mdasim2882 <mohdasim2882@gmail.com>
2024-01-29 03:11:25 +0530

    updated UI: sites card

--- app/src/main/java/com/example/adsensedashboard/ui/fragments/EarningsFragment.kt
+++ app/src/main/java/com/example/adsensedashboard/ui/fragments/EarningsFragment.kt
@@ -1,10 +1,13 @@
 package com.example.adsensedashboard.ui.fragments
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -71,6 +74,7 @@ class EarningsFragment : Fragment() {
     }
 
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun setRecyclerView() {
 
         // Set up the RecyclerView
@@ -88,6 +92,35 @@ class EarningsFragment : Fragment() {
          * */
         val earningsData = listOf("13.56 $", "256.07 $", "2308.46 $", "9008.83 $")
         onEarningsLoadSuccess(earningsData)
+
+        // use arrayadapter and define an array
+        val arrayAdapter: ArrayAdapter<*>
+        val users = arrayOf(
+            "www.google.com", "www.wikipedia.org", "www.amazon.com",
+            "www.stackoverflow.com", "www.cisce.org"
+        )
+
+        // access the listView from xml file
+        var mListView = binding.sitesLayout.userlist
+        arrayAdapter = ArrayAdapter(
+            requireActivity(),
+            android.R.layout.simple_list_item_1, users
+        )
+        mListView.adapter = arrayAdapter
+        mListView.setOnTouchListener({ v, event ->
+            val action = event.action
+            when (action) {
+                MotionEvent.ACTION_DOWN ->                 // Disallow ScrollView to intercept touch events.
+                    v.parent.requestDisallowInterceptTouchEvent(true)
+
+                MotionEvent.ACTION_UP ->                 // Allow ScrollView to intercept touch events.
+                    v.parent.requestDisallowInterceptTouchEvent(false)
+            }
+
+            // Handle ListView touch events.
+            v.onTouchEvent(event)
+            true
+        })
     }
 
     fun onEarningsLoadSuccess(templates: List<String>) {
@@ -96,5 +129,7 @@ class EarningsFragment : Fragment() {
         val largePadding = resources.getDimensionPixelSize(R.dimen.updown_product_grid_spacing)
         val smallPadding = resources.getDimensionPixelSize(R.dimen.side_product_grid_spacing_small)
         recyclerView.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
+
+
     }
 }
