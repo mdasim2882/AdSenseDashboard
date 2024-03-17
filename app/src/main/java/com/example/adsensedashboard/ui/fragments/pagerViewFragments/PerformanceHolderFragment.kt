40e663d55147b5db1340ef53f037a37d48b51b54
mdasim2882 <mohdasim2882@gmail.com>
2024-03-17 16:24:09 +0530

    added APIs endpoint completed: requires value conversion- PerformanceHolderFragment completed

--- app/src/main/java/com/example/adsensedashboard/ui/fragments/pagerViewFragments/PerformanceHolderFragment.kt
+++ app/src/main/java/com/example/adsensedashboard/ui/fragments/pagerViewFragments/PerformanceHolderFragment.kt
@@ -1,33 +1,174 @@
 package com.example.adsensedashboard.ui.fragments.pagerViewFragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.adsensedashboard.R
+import com.example.adsensedashboard.databinding.FragmentPerformanceHolderBinding
+import com.example.adsensedashboard.models.api.PerformanceTest
+import com.example.adsensedashboard.utils.JSON_FORMAT
+import com.example.adsensedashboard.utils.toast
 import com.example.adsensedashboard.viewModels.PerformanceHolderViewModel
+import com.example.adsensedashboard.viewModels.PerformanceHolderViewModelFactory
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
+import com.google.gson.Gson
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class PerformanceHolderFragment : Fragment() {
+    private val TAG = javaClass.name
+    private lateinit var binding: FragmentPerformanceHolderBinding
+    private lateinit var viewModel: PerformanceHolderViewModel
 
     companion object {
         fun newInstance() = PerformanceHolderFragment()
     }
 
-    private lateinit var viewModel: PerformanceHolderViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_performance_holder, container, false)
+        binding = FragmentPerformanceHolderBinding.inflate(layoutInflater, container, false)
+        setupArchitectureComponents()
+        makeAPICall()
+        return binding.root
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(PerformanceHolderViewModel::class.java)
-        // TODO: Use the ViewModel
     }
+
+
+    //region API throttling
+    private fun setupArchitectureComponents() {
+        viewModel = ViewModelProvider(
+            this, PerformanceHolderViewModelFactory(requireActivity().application)
+        ).get(PerformanceHolderViewModel::class.java)
+
+        lifecycle.addObserver(viewModel)
+
+        setupObservers()
+
+    }
+
+    //region ViewModel Observers
+    private fun setupObservers() {
+        viewModel.response.observe(viewLifecycleOwner) {
+            Log.d(TAG, "onCreate: Fetching Response...from Observer")
+            if (it == null) {
+                Log.d(TAG, "onCreate: NULL Response")
+                return@observe
+            }
+            // TODO: Try to use the accountId from here and then call getSites()
+            updateUI(it, "accounts")
+        }
+        viewModel.responseSitesAPI.observe(viewLifecycleOwner) {
+            Log.d(TAG, "onCreate: Fetching responseSitesAPI Response...from Observer")
+            if (it == null) {
+                Log.d(TAG, "onCreate: NULL Response")
+                return@observe
+            }
+            updateUI(it, "sites")
+        }
+    }
+//endregion
+
+    //region Triggering API calls
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun makeAPICall() {
+        GlobalScope.launch {
+            val token = generateAccessToken()
+            Log.d(TAG, "makeAPICall: ACCESS TOKEN : $token")
+            Log.d(
+                TAG,
+                "makeAPICall: SERVER AUTH CODE : ${
+                    GoogleSignIn.getLastSignedInAccount(
+                        requireContext()
+                    )?.serverAuthCode
+                }"
+            )
+            token?.let {
+                val account = viewModel.getAccount("Bearer $token", JSON_FORMAT)
+                account?.let {
+                    val dimensions = listOf<String>()
+                    val metrics = listOf(
+                        "PAGE_VIEWS",
+                        "PAGE_VIEWS_RPM",
+                        "IMPRESSIONS",
+                        "CLICKS",
+                        "COST_PER_CLICK",
+                        "PAGE_VIEWS_CTR"
+                    )
+                    val sites = viewModel.getSites(
+                        account.accounts[0].name.split('/')[1],
+//                        "pub-7890024740593023",
+                        "Bearer $token",
+                        JSON_FORMAT, "YESTERDAY", metrics, dimensions
+                    )
+
+                }
+            }
+
+        }
+    }
+    //endregion
+
+    private fun generateAccessToken(): String? {
+        var token: String? = null
+        try {
+            // Check for existing Google Sign In account, if the user is already signed in
+            // the GoogleSignInAccount will be non-null
+            val currentAccount = GoogleSignIn.getLastSignedInAccount(requireContext())
+            currentAccount?.let { signin ->
+                val credential = GoogleAccountCredential.usingOAuth2(
+                    context, listOf("https://www.googleapis.com/auth/adsense.readonly")
+                ).setSelectedAccount(signin.account)
+
+                // TODO : Setup Backoff if needed
+
+                token = credential.token
+            }
+        } catch (e: Exception) {
+            // TODO("Handle the AdSenseAPI Retrofit response exception")
+            context?.toast("Network issue")
+        }
+        return token
+    }
+
+    private fun <T> updateUI(responseAPI: T, type: String) {
+        Log.d(TAG, "updateUI: Called with ${responseAPI.toString()}")
+        val jsonData = Gson().toJson(responseAPI)
+
+        when (type) {
+            "sites" -> {
+                val resp = responseAPI as PerformanceTest
+
+                binding.pageView.text = String.format("${resp.rows[0].cells[0].value}K")
+                binding.pageRpm.text = String.format("${resp.rows[0].cells[1].value}")
+                binding.pageImpressions.text = String.format("${resp.rows[0].cells[2].value}K")
+                binding.pageClicks.text = String.format("${resp.rows[0].cells[3].value}")
+                binding.pageCpc.text = String.format("${resp.rows[0].cells[4].value}")
+                binding.pageCtr.text = String.format("${resp.rows[0].cells[5].value}")
+            
+            }
+
+            "accounts" -> {
+                // TODO:  Don't require much info, we can reuse account
+                // in a common variable
+            }
+        }
+
+        Log.d(TAG, "updateUI: RESPONSE ON DASHBOARD UI= $jsonData")
+    }
+
+//endregion
+
 
 }
--- app/src/main/java/com/example/adsensedashboard/viewModels/PerformanceHolderViewModel.kt
+++ app/src/main/java/com/example/adsensedashboard/viewModels/PerformanceHolderViewModel.kt
@@ -1,7 +1,94 @@
 package com.example.adsensedashboard.viewModels
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.adsensedashboard.models.RetrofitClient
+import com.example.adsensedashboard.models.api.PerformanceTest
+import com.example.adsensedashboard.models.api.ResponseAdSenseAPI
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class PerformanceHolderViewModel : ViewModel() {
+class PerformanceHolderViewModelFactory(
+    private val app: Application
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return PerformanceHolderViewModel(app) as T
+    }
+}
+
+
+class PerformanceHolderViewModel(app: Application) : AndroidViewModel(app),
+    DefaultLifecycleObserver {
     // TODO: Implement the ViewModel
+    private val TAG = javaClass.name
+    var response = MutableLiveData<ResponseAdSenseAPI?>(null)
+    var responseSitesAPI = MutableLiveData<PerformanceTest?>(null)
+
+    suspend fun getAccount(accessToken: String, accept: String) = withContext(Dispatchers.IO) {
+        try {
+            val accountResp = RetrofitClient.adSenseService.getAccounts(accessToken, accept).body()
+            response.postValue(accountResp)
+
+            Log.d(TAG, "getAccounts: RESPONSE RETROFIT= ${response.value}..")
+
+            // Most probably, it will always be null because we're using
+            // it's value on background thread
+            response.value?.let { res ->
+                Log.d(TAG, "getAccount: DATA FROM CLASS ${res.accounts[0]}")
+                Log.d(TAG, "getAccount: DATA : account_name = ${res.accounts[0].name}")
+            }
+            return@withContext accountResp
+        } catch (e: Exception) {
+            e.printStackTrace()
+            updateUI()
+            null
+        }
+    }
+
+    suspend fun getSites(
+        accountId: String,
+        accessToken: String,
+        accept: String,
+        dateRange: String,
+        metrics: List<String>,
+        dimensions: List<String>
+    ) = withContext(Dispatchers.IO) {
+        try {
+            val accountResp = RetrofitClient.adSenseService.getSitesDetails(
+                accountId,
+                accessToken,
+                accept,
+                dateRange,
+                metrics,
+                dimensions
+            ).body()
+            responseSitesAPI.postValue(accountResp)
+
+            Log.d(
+                TAG,
+                "getSites: RESPONSE SITES RETROFIT= ${response.value}.. accountID : ${accountId} and accountResp : ${accountResp}"
+            )
+
+            // Most probably, it will always be null because we're using
+            // it's value on background thread
+            responseSitesAPI.value?.let { res ->
+                Log.d(TAG, "getSites: DATA FROM CLASS ${res.rows}")
+                Log.d(TAG, "getSites: DATA : Totals = ${res.totals}")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            updateUI()
+            null
+        }
+    }
+
+    private fun updateUI() {
+        // TODO: Update UI in case of Retrofit API call failure
+    }
+
 }
